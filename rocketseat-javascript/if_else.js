3be352a14800716fd60e8426bbc95e2c602c0c05
Washington95 <wst-washington@hotmail.com>
2021-11-26 21:01:40 -0300

    exercicios rocketseat

---
+++ rocketseat-javascript/if_else.js
@@ -0,0 +1,17 @@
+// if.. else
+
+let temperature = 37.2
+let highTemperature = temperature >=37.5
+let mediumTemperature = temperature < 37.5 && temperature >=37
+
+if(highTemperature){
+    console.log('Febre Alta')
+}
+
+else if(mediumTemperature){
+    console.log('Febre Moderada')
+}
+
+else{
+    console.log('Saudável')
+}
