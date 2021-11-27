533e57fc9a704de2247220f634512b0446b9f7bd
Washington95 <wst-washington@hotmail.com>
2021-11-26 21:39:01 -0300

    exercicios rocketseat

---
+++ rocketseat-javascript/throw_try_catch.js
@@ -0,0 +1,20 @@
+// throw
+
+function nome(name = '') {
+    if (name === '') {
+        throw 'Nome é obrigatório'
+    }
+
+    console.log(`Seja bem Vindo ${name}`)
+}
+
+//try ... catch
+try {
+    nome('Gugu')
+} catch (a) {
+    console.log(a)
+}
+
+
+
+console.log('após ao try/catch')
