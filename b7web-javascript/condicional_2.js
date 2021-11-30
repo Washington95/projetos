f4ac58f0ee09c18655d2e7b0fd4de4703ae4b09b
Washington95 <wst-washington@hotmail.com>
2021-11-29 22:51:03 -0300

    condicional (if) ( else if)

---
+++ b7web-javascript/condicional_2.js
@@ -0,0 +1,13 @@
+// condicional horário
+var hora = 18
+
+
+// if( hora >= 12 && hora < 18){
+//     console.log("Boa Tarde !")
+// }
+
+//condicional rush 
+
+if( hora == 12 || hora == 18){
+    console.log("Hora do rush")
+}
