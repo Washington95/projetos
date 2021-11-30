6f640bb9c6ebf69663dc69559eb6807eb8d4ca39
Washington95 <wst-washington@hotmail.com>
2021-11-30 20:10:25 -0300

    elementos de alteração

---
+++ b7web-javascript/elementos.js
@@ -0,0 +1,10 @@
+// document.getElementById("titulo").innerHTML = "Pedro <button>Botao</button>";
+
+//seleciona pelo nome da classe
+document.getElementsByClassName()
+
+//nome da tag
+document.getElementsByTagName()
+
+//acessar atraves do name
+document.getElementsByName()
