2ff7720f2ba2a173cafd69d7bf2ccf21ce5b9302
Washington95 <wst-washington@hotmail.com>
2021-11-29 22:35:53 -0300

    condicional if

---
+++ b7web-javascript/condicional_1.js
@@ -0,0 +1,11 @@
+ //Condicional 
+
+ var hora = 19;
+
+ if (hora < 12) {
+     console.log("Bom Dia !")
+ } else if (hora < 18) {
+     console.log("Boa tarde !")
+ } else {
+     console.log("Boa Noite !")
+ }
