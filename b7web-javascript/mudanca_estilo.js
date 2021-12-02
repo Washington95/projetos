6c6fdda2b39bd634f7c9996e57f3cbd1f7042c34
Washington95 <wst-washington@hotmail.com>
2021-12-01 22:28:48 -0300

    mudanças de estilos

---
+++ b7web-javascript/mudanca_estilo.js
@@ -0,0 +1,27 @@
+// function verde(){
+//     remover();
+//     document.querySelector('#titulo').classList.add('verde');
+
+// }
+// function vermelho(){
+//     remover();
+//     document.querySelector('#titulo').classList.add('vermelho')
+
+// }
+
+// function azul(){
+//     remover();
+//     document.querySelector("#titulo").classList.add('azul')
+// }
+
+// function remover(){
+//     document.querySelector('#titulo').classList.remove("azul")
+//     document.querySelector('#titulo').classList.remove("verde")
+//     document.querySelector('#titulo').classList.remove("vermelho")
+
+// }
+
+function mostrarTelefone(elemento){
+    elemento.style.display = 'none'
+    document.getElementById("telefone").style.display = "block"
+}
