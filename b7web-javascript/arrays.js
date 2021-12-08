599dcb044bcb47ff1615b2dc643268912559ffcd
Washington95 <wst-washington@hotmail.com>
2021-12-08 20:47:15 -0300

    Criando Arrays

---
+++ b7web-javascript/arrays.js
@@ -0,0 +1,15 @@
+// let carros = [
+//     'Palio',
+//     'Uno',
+//     'Corolla',
+//     'Ferrari'
+// ]
+
+// console.log(carros [2])
+
+let ingredientes = [
+    ['uva', 'maça', 'banana'],
+    ['arooz', 'feijao']
+]
+
+console.log(ingredientes[0] [0])
