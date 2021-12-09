dd8c1bf2bcb9cf4590a84599f0ae7383e5e877cc
Washington95 <wst-washington@hotmail.com>
2021-12-09 19:10:39 -0300

    condicional switch

---
+++ b7web-javascript/switch.js
@@ -0,0 +1,39 @@
+let dia = 7;
+let diaNome = '';
+
+// switch(dia){
+//     case 1:
+//         diaNome = 'Segunda-Feira';
+//         break;
+//     case 2:
+//         diaNome = 'Terça-Feira';
+//         break;
+//     case 3:
+//         diaNome = 'Quarta-Feira';
+//         break;
+//     case 4:
+//         diaNome = 'Quinta-Feira';
+//         break;
+//     case 5 :
+//         diaNome = 'Sexta-Feira';
+//         break;
+//     case 6:
+//         diaNome = 'Sábado';
+//         break;
+//     case 7:
+//         diaNome = 'Domingo';
+//         break;
+// }
+
+switch(dia) {
+    case 6:
+    case 7:
+        diaNome = 'Final de Semana';
+        break;
+    default:
+        diaNome = 'Dia da Semana';
+        break ;
+}
+
+document.getElementById('dia').innerHTML = `Hoje é : ${diaNome}`
+
