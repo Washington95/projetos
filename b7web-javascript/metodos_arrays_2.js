d35fc59721c395234b4a53e599db3655cb98eda4
Washington95 <wst-washington@hotmail.com>
2021-12-11 19:38:38 -0300

    metodos arrays 2

---
+++ b7web-javascript/metodos_arrays_2.js
@@ -0,0 +1,34 @@
+// Remove um item da lista
+
+let lista = ['Ovo', 'Banana', 'Maça', 'Uva'];
+
+lista.splice(0, 1);
+
+let res = lista;
+
+console.log(res)
+
+
+//Concatenando Arrays
+
+let lista = ['Ovo', 'Banana', 'Maça', 'Uva'];
+let lista2 = ['Bola', 'Vassoura', 'Fogão']
+
+
+let res = lista.concat(lista2);
+
+console.log(res)
+
+// Ordenar em ordem alfabetica, Inverter a ordem 
+
+let lista = ['Ovo', 'Banana', 'Maça', 'Uva'];
+
+lista.sort();
+lista.reverse();
+
+let res = lista;
+
+console.log(res)
+
+
+
