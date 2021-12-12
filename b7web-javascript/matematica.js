7d879ab9ded365934b9a4ca8a732ad9534b4ac1b
Washington95 <wst-washington@hotmail.com>
2021-12-12 19:59:13 -0300

    matematica

---
+++ b7web-javascript/matematica.js
@@ -0,0 +1,45 @@
+// Arredonda os numeros
+
+let valor = Math.round(3.67);
+
+console.log(valor);
+
+
+//Arredonda pra baixo
+
+let valor = Math.floor(4.99);
+
+console.log(valor);
+
+
+//Arredonda pra cima
+
+let valor = Math.ceil(4.67);
+
+console.log(valor);
+
+//Numero Absoluto
+
+let valor = Math.abs(-3.67);
+
+console.log(valor);
+
+// Me retorna menor valor
+
+let valor = Math.min(10, 15, 30);
+
+console.log(valor);
+
+//Me retorna o maior valor
+
+let valor = Math.max(10, 15, 30);
+
+console.log(valor);
+
+
+//Gerar numeros aleatorios com limite estabelecido
+
+let valor = Math.floor(Math.random() * 100);
+
+console.log(valor);
+
