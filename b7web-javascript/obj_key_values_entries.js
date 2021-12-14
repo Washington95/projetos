f50b2f7fefd7897b5d7af0b959bd9a830b90efee
Washington95 <wst-washington@hotmail.com>
2021-12-13 21:44:58 -0300

    Objetos Key Values Entries

---
+++ b7web-javascript/obj_key_values_entries.js
@@ -0,0 +1,9 @@
+let pessoa = {
+    nome : 'Gugu',
+    sobrenome: 'Luiz',
+    idade: 30
+}
+
+console.log(Object.keys(pessoa));
+console.log(Object.values(pessoa));
+console.log(Object.entries(pessoa));
