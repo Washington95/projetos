ec329d0041cf832f85c9c5156325c2414afdb183
Washington95 <wst-washington@hotmail.com>
2021-12-13 21:35:20 -0300

    includes e repeat

---
+++ b7web-javascript/includes_repeat.js
@@ -0,0 +1,14 @@
+//Retorna True ou False sobre o elemento na lista
+
+let lista = ['ovo', 'feijão', 'café', 'mamão'];
+
+console.log(lista.includes('ovo'))
+
+let nomes = 'Gugu';
+
+console.log(nomes.includes('h'))
+
+let nome = 'Gugu ';
+
+console.log(nome.repeat(5))
+
