f17e81c5819cdab61eca8eefd6087fc299c88881
Washington95 <wst-washington@hotmail.com>
2021-12-15 20:37:06 -0300

    Callback

---
+++ b7web-javascript/callback.js
@@ -0,0 +1,9 @@
+function alertar (){
+    console.log("Oi, tudo bem ?");
+}
+
+let nome = 'Gugu';
+setTimeout(alertar, 1000);
+let sobrenome = 'Luiz';
+console.log(`Nome completo: ${nome} ${sobrenome}`)
+
