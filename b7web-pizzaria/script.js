f086a63944a7e439dfe35dac744f6c3b4e50e429
Washington95 <wst-washington@hotmail.com>
2022-01-03 20:58:20 -0300

    projeto pizzaria parte 5

--- b7web-pizzaria/script.js
+++ b7web-pizzaria/script.js
@@ -4,7 +4,7 @@ const qa = (el)=>document.querySelectorAll(el);
 
 pizzaJson.map((item, index)=>{
     let pizzaitem = q('.pizza-item').cloneNode(true);
-
+    pizzaitem.setAttribute('data-key', index);
     pizzaitem.querySelector('.pizza-item--img img').src = item.img;
     pizzaitem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
     pizzaitem.querySelector('.pizza-item--name').innerHTML = item.name;
@@ -12,6 +12,13 @@ pizzaJson.map((item, index)=>{
 
     pizzaitem.querySelector('a').addEventListener('click', (e)=>{
         e.preventDefault();
+
+        let key = e.target.closest('.pizza-item').getAttribute('data-key');
+
+        q('.pizzaBig img').src = pizzaJson[key].img;
+        q('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        q('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+        q('.pizzaInfo--actualPrice').innerHTML = pizzaJson[key].price.toFixed(2)
 
         q('.pizzaWindowArea').style.opacity = 0;
         q('.pizzaWindowArea').style.display = 'flex';
@@ -22,4 +29,4 @@ pizzaJson.map((item, index)=>{
     });
 
     q('.pizza-area').append(pizzaitem);
-});
+});  
