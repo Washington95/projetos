a3fb9b0dcfbb88edb4d57a30c5c8accc6b4065ad
Washington95 <wst-washington@hotmail.com>
2022-01-03 20:09:34 -0300

    projeto pizzaria parte 3

--- b7web-pizzaria/script.js
+++ b7web-pizzaria/script.js
@@ -3,7 +3,12 @@ const qa = (el)=>document.querySelectorAll(el);
 
 
 pizzaJson.map((item, index)=>{
-    let pizzaitem = q(' .pizza-item').cloneNode(true);
+    let pizzaitem = q('.pizza-item').cloneNode(true);
+
+    pizzaitem.querySelector('.pizza-item--img img').src = item.img;
+    pizzaitem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+    pizzaitem.querySelector('.pizza-item--name').innerHTML = item.name
+    pizzaitem.querySelector('.pizza-item--desc').innerHTML = item.description
 
     q('.pizza-area').append(pizzaitem);
 });
